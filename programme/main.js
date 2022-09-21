eb7f5fc7701ba22884a3f0a4baa8e51295b4ab4c
Ayrton <totone45340@gmail.com>
2022-09-21 11:30:04 +0200

    afficher le tab en ligne de commande

--- programme/main.js
+++ programme/main.js
@@ -1,8 +1,15 @@
 var puissance4 = [];
 var nbColonne = 7;
 var nbLigne = 6;
+var joueur1 = 'x';
+var joueur2 = 'o';
+
 puissance4 = initTabVite(nbLigne,nbColonne,0);
-console.log(puissance4)
+afficherPuissance4(puissance4),joueur1,joueur2;
+puissance4[3][3] = 1;
+puissance4[4][4] = 2;
+afficherPuissance4(puissance4),joueur1,joueur2;
+
 /**
  * permet d'initialiser un tableau de tableau en fonction d'un nombre de ligne et de colonne passé en paramètre
  * @param {number} nbColonne 
@@ -20,4 +27,28 @@ function initTabVite(nbColonne,nbLigne,car = ''){
         tab.push(ligne);
     }
     return tab;
+}
+/**
+ *Perm et d'afficher un tableau de puissance 4
+ * @param {Array<string>} tab 
+ * @param {string} joueur1 
+ * @param {string} joueur2 
+ */
+
+function afficherPuissance4(tab,joueur1,joueur2){
+    for(var i=0; i < tab.length; i++){
+        var ligne = "";
+        for(var j=0; j < tab[i].length; j++){
+            ligne += '| '
+            if(tab[i][j] === 0){
+                ligne += '_'
+            } else if(tab[i][j] === 1){
+                ligne += joueur1
+            } else if(tab[i][j] === 2){
+                ligne += joueur2;
+            }
+            ligne += ' |'
+        }
+        console.log(ligne)
+    }
 }
